a9fba1f1bb5256e814bb83894d14a2fb0b063a37
Jessica Paul <paul.jessica95@gmail.com>
2021-03-23 12:41:00 -0400

    add db connection url to config vars

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const methodOverride = require("method-override");
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 const indexRouter = require("./routes/index.js");
 const weeksRouter = require("./routes/weeks.js");
 const daysRouter = require("./routes/days.js");
